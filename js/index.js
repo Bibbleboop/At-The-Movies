3e00c20f370ba335f69312067b5d6bffff5b537e
Bibbleboop <nicjah@hotmail.com>
2023-02-23 12:33:54 +0000

    search btn tests

--- js/index.js
+++ js/index.js
@@ -13,45 +13,47 @@
 // TEST with first 150 instead of entire pokemon list before run all data
 const pokemonCount = 10;
 
-// ****** check if this works
-//to clear th escreen for new search
-// let clearSearch = function () {
-//     for (let type of pokemonsChars) {
-//         console.log(type)
-//     }
-// }
+
 
 let pokemonsChars = document.getElementById("name" + "types" + "sprites/front/shiny");
 
 // Show Pokemon API Url to use from Pokeapi.io ensuring using latest version
 // let url = "https://pokeapi.co/api/v2/pokemon/{ + "name/" + "types/" + "sprites/front/shiny"};
 
-let url = "https://pokeapi.co/api/v2/pokemon/item/master-ball/"
-
+// let url = "https://pokeapi.co/api/v2/pokemon/item/master-ball/"
+// GET https://pokeapi.co/api/v2/ability/{id or name}/
+let url ="https://pokeapi.co/api/v2/ability/{picachu}/"
 // Matching with lables in Resource List  
 let pokemonsName = document.getElementById("name");
 let pokemonsTypes = document.getElementById("types");
 let pokemonsImg = document.getElementById("sprites.front_shiny");
 
+// namebtn  - when enter characher name eg Pickachu  - to search using API
+function pokemonsName() {
+    let x document.getElementById("namebtn");
+    document.getElementById().innerHTML ="You chose: " + x.value;
+}
+
 //Buttons used to input and select 
-let namebtn = document.getElementById("button");
+let namebtn = document.getElementById("pokemonsName");
 console.log(namebtn);
 let input = document.getElementById("userinput");
 console.log(input);
 let randombtn = document.getElementById("random");
 console.log(randombtn);
 // Fetch API fetch the URL (promise in js) This gets the raw data that is transformed into JSON - also catches & console logs any errors
-// let searchPokemon = function (apiobj) {
-// fetch(url)
-// .then((rawdata) => rawdata.json())
-// .then((data) => addHTML(data))
-// .catch((error) => {
-    // if an error occurs, the following message displayed to advise next steps pokemonhtml.innerHTML = '<h1> Error Alert. Time to check your code! </h1>
-//     console.log(error)})
-// }
-// API response required to fetch from database using await as asynchronus
-let response = fetch(url); 
-console.log("Pokemon Characters" + pokemonsChars);
+let searchPokemon = function (apiobj) {
+get(url)
+.then((rawdata) => rawdata.json())
+.then((data) => addHTML(data))
+.catch((error) => {
+    // if error occurs, the following message displayed to advise next steps 
+    pokemonhtml.innerHTML = '<h1> Error Alert. Time to check your code! </h1>'
+    console.log(error)})
+}
+// // API response required to fetch from database using await as asynchronus
+// let response = get(url); 
+// console.log("Pokemon Characters" + pokemonsChars);
 // let pokemonChars = response.json();
 //async function 
 // let pokemonChars = await fetch(url);
@@ -68,6 +70,14 @@ console.log("Pokemon Characters" + pokemonsChars);
 // Add event listener 
 
 
+// ****** check if this works
+//to clear th escreen for new search
+// let clearSearch = function () {
+//     for (let type of pokemonsChars) {
+//         console.log(type)
+//     }
+// }
+
 //To place results in HTML page using DOM
 let pokemonHtml = document.querySelector('.pokemon')
 //Adds a variable to the values from API
@@ -76,11 +86,11 @@ let toAddin = 'My Name is : ${data.name}, whats yours?'
 // pokemonHtml.innerHTML = to Addin ****** check this 
 
 
-//Try this to make button for searches-->
-//Add event handler -->
-button.addEventListener ("click", function() {
-  alert("did something");
-});
+// //Try this to make button for searches-->
+// //Add event handler -->
+// button.addEventListener ("click", function() {
+//   alert("did something");
+// });
 
 
 // Local storage
