bae52f3a8aec7ad712a793cc85cef910eb89d643
Bibbleboop <nicjah@hotmail.com>
2023-03-09 20:54:42 +0000

    remove error link

--- js/index.js
+++ js/index.js
@@ -86,7 +86,7 @@ let getfilmdata = function (film) {
 
 
 // <!-- Load icon library -->
-<link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
 
 
 
